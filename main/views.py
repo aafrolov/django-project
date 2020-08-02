74911210f3a4bf2e95443c2a3a88b52479b26415
Andrey Frolov <fatboy911@yandex.ru>
2020-08-02 19:46:55 +0300

    Проект Django, работа с БД

---
+++ main/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from .models import Task
+
+
+# Create your views here.
+def index(request):
+    tasks = Task.objects.all()
+    return render(request, 'main/index.html', {'title': 'Главная страниыца сайта', 'tasks': tasks})
+
+
+def about(request):
+    return render(request, 'main/about.html')
